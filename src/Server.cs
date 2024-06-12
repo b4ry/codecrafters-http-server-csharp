834ad586702b1a484bd59fa3a456a67fa26b093e
b4ry <barry90@interia.pl>
2024-06-12 12:37:33 +0200

    Send 200 response. Add hacky task delay to make sure the client received.

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,23 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+using (var socket = server.AcceptSocket())
+{
+    var response = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+
+    try
+    {
+        await socket.SendAsync(response);
+        await Task.Delay(100);
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine($"Exception while sending a response back! {ex}");
+    }
+}
+
+server.Stop();

4047216caf7ac28caeb2fbd5ffe4478f309c631c
b4ry <barry90@interia.pl>
2024-06-13 14:47:41 +0200

    Let's debug

--- src/Server.cs
+++ src/Server.cs
@@ -72,11 +72,11 @@ while (true)
                     {
                         response.ContentEncoding = "gzip";
                         
-                        var stream = new MemoryStream();
-                        var gzipStream = new GZipStream(stream, CompressionLevel.Optimal, leaveOpen: true);
-
+                        using var stream = new MemoryStream();
+                        using (var gzipStream = new GZipStream(stream, CompressionLevel.Optimal, leaveOpen: true))
+                        {
                             await gzipStream.WriteAsync(Encoding.ASCII.GetBytes(requestArgument));
-
+                        }
 
                         stream.Position = 0;
                         requestArgument = Encoding.ASCII.GetString(stream.ToArray());
